1dffcefe3e665d3cf628df522e81068c72564066
FrankCostta <fransta8@gmail.com>
2022-07-20 22:50:27 -0300

    Atualizando

---
+++ trainning/src/components/Catalogo.js
@@ -0,0 +1,12 @@
+import './Catalogo.css';
+
+function Catalogo() {
+	return (
+		<div className="Catalogo">
+			<div className='theme'></div>
+			<div className='description'>Description</div>
+		</div>
+	);
+}
+
+export default Catalogo;
